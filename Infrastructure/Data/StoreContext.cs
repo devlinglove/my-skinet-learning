d62f697ae37c215cee25b1144d66eb33d5fe3fc5
Umar <devlinglove@gmail.com>
2025-06-28 08:43:42 +0500

    End of section 2

---
+++ Infrastructure/Data/StoreContext.cs
@@ -0,0 +1,24 @@
+﻿using Core.Entities;
+using Infrastructure.Data.Config;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace Infrastructure.Data
+{
+    public class StoreContext : DbContext
+    {
+        public StoreContext(DbContextOptions options) : base(options)
+        {
+        }
+        public DbSet<Product> Products { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(ProductConfiguration).Assembly);
+        }
+
+
+    }
+}
