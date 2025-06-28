03276d118c81aab3f6fd6fb57c4ca9be2ad0c7b3
Umar <devlinglove@gmail.com>
2025-06-28 11:32:15 +0500

    seeding products

---
+++ Infrastructure/Data/StoreContextSeed.cs
@@ -0,0 +1,27 @@
+﻿
+
+using Core.Entities;
+using System.Text.Json;
+
+namespace Infrastructure.Data
+{
+    public class StoreContextSeed
+    {
+        public static async Task SeedAsync(StoreContext context)
+        {
+            if (!context.Products.Any()) {
+
+                var products = await File.ReadAllTextAsync("../Infrastructure/Data/SeedData/products.json");
+
+                var jsonProducts = JsonSerializer.Deserialize<List<Product>>(products);
+
+                if (jsonProducts == null) return;
+
+                context.AddRange(jsonProducts);
+
+                await context.SaveChangesAsync();   
+
+            }
+        }
+    }
+}
